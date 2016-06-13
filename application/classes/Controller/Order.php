1cf4618c9111de84de6978c59e7cab7931fdff38
xrh <540693750@qq.com>
2016-06-13 20:30:22 +0800

    order

--- application/classes/Controller/Order.php
+++ application/classes/Controller/Order.php
@@ -12,7 +12,18 @@
  	 	 */	
  		public function action_create() {
  			$params = json_decode(Arr::get($_POST, 'params', ''), true);
-
+ 			$lotteryId = Arr::get($params, 'lottoryID', 0);
+ 			$ticketCount = Arr::get($params, 'ticketCount', 0);
+			if(!$ticketCount) {
+				return $this->failed(900002);
+			}
+			$createSn = $this->createSn();
+			
+			
+ 		}
+ 		
+ 		private function createSn() {
+    		return date("YmdHis" ).str_pad( mt_rand( 1, 99999 ), 5, "0", STR_PAD_LEFT );
  		}
  }
 
