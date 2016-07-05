ac6dabe17a057feedd18599cfddd14ad3b37b6d4
Freud <lixiangyujiayou@gmail.com>
2016-07-05 22:15:35 +0800

    Fix:修正错误

--- application/classes/Business/Period.php
+++ application/classes/Business/Period.php
@@ -3,7 +3,10 @@
  * Created on 2016
  * 期彩期数逻辑层
  */
- class Business_Period extends Business { 
+ class Business_Period extends Business {
+	 public function getOnlineLotteryByGoodsId($goodsId) {
+		 return Dao::factory('Period')->getOnlineLotteryByGoodsId($goodsId);
+	 }
 
  	public function getLotteryCountByGoodsId($sid) {
  		return Dao::factory('Period')->getLotteryCountByGoodsId($sid);
